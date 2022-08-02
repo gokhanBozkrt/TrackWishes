386b74a3c8e98ab4fb86c214bf28586abfa8dab5
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-08-02 15:03:25 +0300

    Initial Commit

---
+++ TrackWishes/TrackWishesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackWishesApp.swift
+//  TrackWishes
+//
+//  Created by Gokhan Bozkurt on 2.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrackWishesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

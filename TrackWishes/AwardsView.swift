767a242d1e27a9dc37fca3c529dc82aea891134b
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-08-18 22:57:18 +0300

    - Create a method in the data controller class to check award earned or not - Add alert to awards view

--- TrackWishes/AwardsView.swift
+++ TrackWishes/AwardsView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct AwardsView: View {
     static let tag: String? = "Awards"
+    @EnvironmentObject var dataController: DataController
+    
+    @State private var selectedAward = Award.example
+    @State private var showingAlertDetails = false
+    
     var columns: [GridItem] {
         [GridItem(.adaptive(minimum: 100, maximum: 100))]
     }
@@ -19,21 +24,28 @@ struct AwardsView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(Award.allAwards) { award in
                         Button {
-                            
+                            selectedAward = award
+                            showingAlertDetails = true
                         } label: {
                             Image(systemName: award.image)
                                 .resizable()
                                 .scaledToFit()
                                 .padding()
                                 .frame(width: 100, height: 100)
-                               .foregroundColor(Color.secondary.opacity(0.5))
-                               
+                               .foregroundColor(
+                                dataController.hasEarned(award: award) ? Color(award.color) : Color.secondary.opacity(0.5))
                             
                         }
 
                     }
                 }
-            }.navigationTitle("Awards")
+            }
+            .navigationTitle("Awards")
+        }
+        .alert(dataController.hasEarned(award: selectedAward) ? "Unlock:\(selectedAward.name)" : "Locked" , isPresented: $showingAlertDetails) {
+            Button("OK") { }
+        } message: {
+            Text("\(selectedAward.description)")
         }
     }
 }

f7c39aa2b028c9aa9ffa1d02f8dc557e64a40936
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-08-20 14:19:29 +0300

    - Add Color extension from UKit - Create home view

---
+++ TrackWishes/Color-Addittion.swift
@@ -0,0 +1,30 @@
+//
+//  Color-Addittion.swift
+//  TrackWishes
+//
+//  Created by Gokhan Bozkurt on 20.08.2022.
+//
+
+import SwiftUI
+
+
+
+extension Color {
+  static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+   static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
+
+
+
+
+//
+// extension ShapeStyle where Self == Color {
+//     static var darkBackground: Color {
+//         Color(red: 0.1, green: 0.1, blue: 0.2)
+//     }
+//     static var lightBackground: Color {
+//         Color(red: 0.2, green: 0.2, blue: 0.3)
+//     }
+// }
+
+ 

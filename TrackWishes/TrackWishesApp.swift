90f0b543099a9d8f30483664e64d07514070f288
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-08-30 16:23:32 +0300

    - Document some part of the project

--- TrackWishes/TrackWishesApp.swift
+++ TrackWishes/TrackWishesApp.swift
@@ -3,7 +3,7 @@
 //  TrackWishes
 //
 //  Created by Gokhan Bozkurt on 2.08.2022.
-//
+
 
 import SwiftUI
 
@@ -20,6 +20,10 @@ struct TrackWishesApp: App {
             ContentView()
                 .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(dataController)
+                // Automatically save when we detect that we are
+                // no longer the foreground app. Use this rather than
+               // scene phase so we can port to macOS, where scene
+              // phase won't detect our app losing focus.
                 .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification), perform: save)
         }
     }
